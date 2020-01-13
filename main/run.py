7a935503a82654bbd5e008d5a125186f971b2c32
tzaton <58374797+tzaton@users.noreply.github.com>
2020-01-13 23:26:22 +0100

    Added logging and data export

--- main/run.py
+++ main/run.py
@@ -1,13 +1,31 @@
+import logging
+from pathlib import Path
+
 from main.scraper import OLXScraper
 
 
 selected_filters = {'Umeblowane': 'Tak',
                     'Liczba pokoi': ('2 pokoje', '3 pokoje'),
-                    'Cena do': '800000',
+                    'Cena do': '700000',
                     'Dzielnica': ['Ochota', 'Mokotów'],
                     'Pow. od': '40'
                     }
 
+
 if __name__ == '__main__':
+    # Logging configuration
+    log_file = Path(__file__).parent.parent / 'log' / 'flat_scraper.log'
+    log_formatter = logging.Formatter(fmt='%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s',
+                                      datefmt='%Y-%m-%d %H:%M:%S')
+    sh = logging.StreamHandler()
+    fh = logging.FileHandler(filename=log_file, mode='w')
+    sh.setFormatter(log_formatter)
+    fh.setFormatter(log_formatter)
+    logging.basicConfig(level=logging.DEBUG,
+                        handlers=[sh, fh])
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+    # Run scraper
     scraper = OLXScraper(selected_filters)
     scraper.run()
+    scraper.export_data("olx_test.json")
